83830c17d638b464a7a8bc61b73ecdf47f7c6f07
Inspiravetion <charlie.lipford.code@gmail.com>
2014-02-25 03:19:04 -0500

    SCSS and Jasmine Test build steps added

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -5,24 +5,49 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var stripDebug = require('gulp-strip-debug');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var jasmine = require('gulp-jasmine');
  
-// JS concat, strip debugging and minify
+///////////////////////////////////////
+//               Tasks               //
+///////////////////////////////////////
+
 gulp.task('scripts', function() {
             //can specify order for deps
-  gulp.src([/*'./src/scripts/lib.js',*/'./dev/scripts/*.js'])
+  var stream = gulp.src([/*'./src/scripts/lib.js',*/'./dev/scripts/*.js'])
     .pipe(concat('script.js'))
     .pipe(stripDebug())
     .pipe(uglify())
     .pipe(gulp.dest('./prod/scripts/'));
+
+  console.log(stream);
 });
 
+gulp.task('styles', function() {
 
-/**
- * Startup task watchers
- */
+  gulp.src(['./dev/styles/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('./prod/styles/*.scss'));
+
+});
+
+gulp.task('tests', function() {
+
+  gulp.src('./dev/spec/*.js')
+    .pipe(jasmine());
+
+});
+
+///////////////////////////////////////
+//        Default Build Script       //
+///////////////////////////////////////
+
 gulp.task('default', function(){
 
-    //script compilation
-    gulp.watch('./dev/scripts/*.js', ['scripts']);
+    //JS Compilation
+    gulp.watch('./dev/scripts/*.js', ['scripts', 'tests']);
+
+    //CSS Compilation
+    gulp.watch('./dev/styles/*.scss', ['styles']);
 
 });
